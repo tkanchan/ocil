bc792f5d79325fad36d0ad6126c9ecad2ede8117
Tanes <tkanchanawanchai@gmial.com>
2012-08-12 15:48:02 -0400

    test

---
+++ OCIL/src/com/itevo/ocil/Test.java
@@ -0,0 +1,14 @@
+package com.itevo.ocil;
+public class Test {
+	protected String name;
+	public Test(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
